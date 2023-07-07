5d285d3a34485810f840e5cce0690118c2a7a2ab
Kewei Yan <yankw62@gmail.com>
2023-07-07 15:10:34 -0500

    Update code
    - Add options for loading checkpoints from backbone
    - Add options for loading weights from blocks only

--- dinov2/train/ssl_meta_arch.py
+++ dinov2/train/ssl_meta_arch.py
@@ -48,7 +48,21 @@ class SSLMetaArch(nn.Module):
         if cfg.student.pretrained_weights:
             chkpt = torch.load(cfg.student.pretrained_weights)
             logger.info(f"OPTIONS -- pretrained weights: loading from {cfg.student.pretrained_weights}")
-            student_backbone.load_state_dict(chkpt["model"], strict=False)
+            #student_backbone.load_state_dict(chkpt["model"], strict=False)
+###################################################
+# load weights from backbone and load blocks only #
+###################################################
+            chkpt_blks = {k : v for k, v in chkpt.items() if k.startswith('blocks')}
+#### match the keys of blocks, if needed
+            #chkpt_blks_renamed = {}
+            #for k, v in chkpt_blks.items():
+            #    k2 = k.replace('blocks.', 'blocks.0.')
+            #    chkpt_blks_renamed[k2] = v
+#### ignore the unmatched keys
+            #student_backbone.load_state_dict(chkpt_blks_renamed)
+            #student_backbone.load_state_dict(chkpt_blks_renamed, strict=False)
+            #student_backbone.load_state_dict(chkpt_blks)
+            student_backbone.load_state_dict(chkpt_blks, strict=False)
 
         self.embed_dim = embed_dim
         self.dino_out_dim = cfg.dino.head_n_prototypes
